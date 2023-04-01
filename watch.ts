2b13ce334d042de9915e39c42fc20701f54ee002
Javier Buron <dev@buron.mx>
2023-03-31 19:14:31 -0600

    Add watch.js

---
+++ watch.ts
@@ -0,0 +1,67 @@
+import type {InlineConfig, ViteDevServer} from 'vite'
+import {build, createLogger, createServer} from 'vite'
+import electronPath from 'electron'
+import {ChildProcessWithoutNullStreams, spawn} from 'child_process'
+
+// Shared config across multiple build watchers.
+const sharedConfig: InlineConfig = {
+  mode: 'development',
+  build: {watch: {}},
+}
+
+/**
+ * Create a Vite build watcher that automatically recompiles when a file is
+ * edited.
+ */
+const getWatcher = (name: string, configFilePath: string, writeBundle: any) =>
+  build({
+    ...sharedConfig,
+    configFile: configFilePath,
+    plugins: [{name, writeBundle}],
+  })
+
+
+/**
+ * Set up a watcher for the preload package.
+ */
+const setupPreloadWatcher = async (viteServer: ViteDevServer) =>
+  getWatcher('reload-app-on-preload-package-change', 'app/preload/vite.config.ts', () => {
+    // Send a "full-reload" page event using Vite WebSocket server.
+    viteServer.ws.send({type: 'full-reload'})
+  })
+
+/**
+ * Set up the `back` watcher.
+ */
+const setupMainWatcher = async () => {
+  const logger = createLogger('info', {prefix: '[back]'})
+  let spawnProcess: ChildProcessWithoutNullStreams | null = null
+
+  return getWatcher('reload-app-on-back-package-change', 'app/back/vite.config.ts', () => {
+    if (spawnProcess !== null) {
+      spawnProcess.off('exit', () => process.exit)
+      spawnProcess.kill('SIGINT')
+      spawnProcess = null
+    }
+
+    // Restart Electron process when main package is edited and recompiled.
+    spawnProcess = spawn(String(electronPath), ['.'])
+  })
+}
+
+(async () => {
+  try {
+    const rendererServer = await createServer({
+      ...sharedConfig,
+      configFile: 'app/front/vite.config.ts',
+    })
+
+    await rendererServer.listen()
+    rendererServer.printUrls()
+
+    await setupPreloadWatcher(rendererServer)
+    await setupMainWatcher()
+  } catch (err) {
+    console.error(err)
+  }
+})().catch((err) => console.error(err))
