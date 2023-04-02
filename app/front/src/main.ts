f70cbbc3cfa82aadfce48006fda024685515c212
Javier Buron <dev@buron.mx>
2023-04-02 12:50:57 -0600

    Add bootstrap scripts to main.ts

--- app/front/src/main.ts
+++ app/front/src/main.ts
@@ -1,4 +1,6 @@
-import { createApp } from 'vue'
+import {createApp} from 'vue'
+import '@popperjs/core'
+import 'bootstrap'
 import './style.css'
 import './assets/app.scss'
 import App from './App.vue'
