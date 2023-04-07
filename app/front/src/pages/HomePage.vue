197bdda8c0c2eb45853a991c586b1a3f9de1d1ef
Javier Buron <dev@buron.mx>
2023-04-07 11:56:20 -0600

    Move Qr from HomePage.vue to QrPage.vue

--- app/front/src/pages/HomePage.vue
+++ app/front/src/pages/HomePage.vue
@@ -1,24 +1,9 @@
 <template>
   <admin-layout>
     Hi from home!
-    <button class="btn btn-primary" type="button" @click="generateQr">Generate QR</button>
-    <div v-html="htmlSvg"></div>
   </admin-layout>
 </template>
 
 <script setup lang="ts">
 import AdminLayout from '../layouts/AdminLayout.vue'
-import {ref} from "vue";
-import DOMPurify from 'dompurify';
-import {QrService} from "../services/qr-service";
-import {ServiceFactory} from "../factories/service-factory";
-
-const qrService = ServiceFactory.create<QrService>(QrService)
-const htmlSvg = ref<string>('');
-
-async function generateQr(): Promise<void> {
-  const svg = await qrService.generate()
-  htmlSvg.value = DOMPurify.sanitize(svg)
-}
-
 //</script>
--- app/front/src/pages/QrPage.vue
+++ app/front/src/pages/QrPage.vue
@@ -1,3 +1,23 @@
 <template>
-  QR!
+  <admin-layout>
+    <button class="btn btn-primary" type="button" @click="generateQr">Generate QR</button>
+    <div v-html="htmlSvg"></div>
+  </admin-layout>
 </template>
+
+<script setup lang="ts">
+import {ref} from "vue";
+import DOMPurify from 'dompurify';
+import {QrService} from "../services/qr-service";
+import {ServiceFactory} from "../factories/service-factory";
+import AdminLayout from "../layouts/AdminLayout.vue";
+
+const qrService = ServiceFactory.create<QrService>(QrService)
+const htmlSvg = ref<string>('');
+
+async function generateQr(): Promise<void> {
+  const svg = await qrService.generate()
+  htmlSvg.value = DOMPurify.sanitize(svg)
+}
+
+//</script>
