29cac7014fc4613de13d484548a6027401dbf531
Javier Buron <dev@buron.mx>
2023-04-07 11:06:48 -0600

    Create method hardcoded generate in qr-controller

--- app/back/src/controllers/qr-controller.ts
+++ app/back/src/controllers/qr-controller.ts
@@ -1,7 +1,14 @@
 import {IpcMainInvokeEvent} from 'electron'
+import {ResponseModel} from "../models/response-model";
+import QRCode from 'qrcode'
 
 export class QrController {
-  public async generate(event: IpcMainInvokeEvent, ...args: any[]): Promise<string> {
-    return ''
+  public async generate(event: IpcMainInvokeEvent, ...args: any[]): Promise<ResponseModel<string>> {
+    try {
+      const foo = await QRCode.toString('Andy', {type: 'svg'});
+      return ResponseModel.doSuccess<string>(foo)
+    } catch (e) {
+      console.error(e)
+    }
   }
 }
