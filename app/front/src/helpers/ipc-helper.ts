28ef5c15933fc505041a67f206568b23c176a431
Javier Buron <dev@buron.mx>
2023-04-07 11:19:16 -0600

    Fix ipc-helper.ts

--- app/front/src/helpers/ipc-helper.ts
+++ app/front/src/helpers/ipc-helper.ts
@@ -1,14 +1,14 @@
 export class IpcHelper {
-  private static _instance?: IpcHelper
+  private static _instance: IpcHelper | null
 
   public static get instance(): IpcHelper {
-    if (this._instance != null) {
-      return this._instance
+    if (this._instance == null) {
+      this._instance = new IpcHelper()
     }
-    return new IpcHelper()
+    return this._instance;
   }
 
-  public static async invoke(channel: string, ...args: any[]): Promise<any> {
+  public async invoke(channel: string, ...args: any[]): Promise<any> {
     return await window.electronAPI.invoke(`back:${channel}`, ...args)
   }
 }
