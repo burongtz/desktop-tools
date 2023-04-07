c4921736305824f983672a3ead7150ea27a43670
Javier Buron <dev@buron.mx>
2023-04-07 11:07:14 -0600

    Generate qr in home

--- app/front/src/pages/HomePage.vue
+++ app/front/src/pages/HomePage.vue
@@ -2,15 +2,23 @@
   <admin-layout>
     Hi from home!
     <button class="btn btn-primary" type="button" @click="generateQr">Generate QR</button>
+    <div v-html="htmlSvg"></div>
   </admin-layout>
 </template>
 
 <script setup lang="ts">
 import AdminLayout from '../layouts/AdminLayout.vue'
-import {IpcHelper} from "../helpers/ipc-helper";
+import {ref} from "vue";
+import DOMPurify from 'dompurify';
+import {QrService} from "../services/qr-service";
+import {ServiceFactory} from "../factories/service-factory";
+
+const qrService = ServiceFactory.create<QrService>(QrService)
+const htmlSvg = ref<string>('');
 
 async function generateQr(): Promise<void> {
-  await IpcHelper.invoke('qrs.generate')
+  const svg = await qrService.generate()
+  htmlSvg.value = DOMPurify.sanitize(svg)
 }
 
 //</script>
