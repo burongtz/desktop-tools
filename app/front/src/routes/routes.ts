829839abdd637d2e5a939715541fd7c81eb3362e
Javier Buron <dev@buron.mx>
2023-04-07 11:42:08 -0600

    Add routes.ts

---
+++ app/front/src/routes/routes.ts
@@ -0,0 +1,24 @@
+import {createRouter, createWebHistory} from 'vue-router'
+
+const routes = [{
+  path: '/',
+  alias: '/home',
+  name: 'home',
+  component: () => import('../pages/HomePage.vue'),
+}, {
+  path: '/qrs/create',
+  name: 'qrs.create',
+  component: () => import('../pages/QrPage.vue')
+}, {
+  path: '/404',
+  name: 'errors.not-found',
+  component: () => import('../pages/errors/Error404Page.vue'),
+}, {
+  path: '/:pathMatch(.)',
+  redirect: {name: 'errors.not-found'}
+}]
+
+export const router = createRouter({
+  history: createWebHistory('/'),
+  routes
+})
