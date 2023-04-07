1f9d197a96c67034989d6521b6c2c588f5fc2e5f
Javier Buron <dev@buron.mx>
2023-04-06 23:05:23 -0600

    Call handled channel qrs.generate

--- app/front/src/pages/HomePage.vue
+++ app/front/src/pages/HomePage.vue
@@ -1,9 +1,15 @@
 <template>
   <admin-layout>
     Hi from home!
+    <button class="btn btn-primary" type="button" @click="generateQr">Generate QR</button>
   </admin-layout>
 </template>
 
 <script setup lang="ts">
 import AdminLayout from '../layouts/AdminLayout.vue'
+
+async function generateQr(): Promise<void> {
+  await IpcHelper.invoke('qrs.generate')
+}
+
 //</script>
