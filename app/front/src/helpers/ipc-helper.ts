766b6c78f37f36ffaad516fb86e477f712705ff4
Javier Buron <dev@buron.mx>
2023-04-07 11:03:49 -0600

    Add getter instance to ipc-helper.ts

--- app/front/src/helpers/ipc-helper.ts
+++ app/front/src/helpers/ipc-helper.ts
@@ -1,4 +1,13 @@
 export class IpcHelper {
+  private static _instance?: IpcHelper
+
+  public static get instance(): IpcHelper {
+    if (this._instance != null) {
+      return this._instance
+    }
+    return new IpcHelper()
+  }
+
   public static async invoke(channel: string, ...args: any[]): Promise<any> {
     return await window.electronAPI.invoke(`back:${channel}`, ...args)
   }
