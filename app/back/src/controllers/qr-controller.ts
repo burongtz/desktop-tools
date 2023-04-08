4987685a7511b88b37be393b6ad2309a6197f367
Javier Buron <dev@buron.mx>
2023-04-08 09:20:11 -0600

    Generate qr in back from front input

--- app/back/src/controllers/qr-controller.ts
+++ app/back/src/controllers/qr-controller.ts
@@ -5,7 +5,14 @@ import QRCode from 'qrcode'
 export class QrController {
   public async generate(event: IpcMainInvokeEvent, args: object): Promise<ResponseModel> {
     try {
-      const qr = await QRCode.toString(args.content, {type: 'svg'});
+      const qr = await QRCode.toString(
+        args.content,
+        {
+          type: 'svg',
+          errorCorrectionLevel: 'H',
+          margin: 1
+        }
+      )
       return ResponseModel.doSuccess<string>(qr)
     } catch (e: any) {
       return ResponseModel.doError(e.message);
